bb7da5b0e1a033862a5b13e3d472ece18eb8f3db
Streak69 <pranaykumarpanda00@gmail.com>
2022-12-16 09:53:18 +0530

    warnings fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import decode from 'jwt-decode';
@@ -17,11 +17,11 @@ const Navbar = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const handleLogout = () => {
+    const handleLogout = useCallback(() => {
         dispatch({ type: 'LOGOUT'});
         navigate('/');
         dispatch(setCurrentUser(null));
-    }
+    }, [dispatch,navigate])
 
     useEffect(() => {
         const token = User?.token
@@ -32,7 +32,7 @@ const Navbar = () => {
             }
         }
         dispatch(setCurrentUser(JSON.parse(localStorage.getItem('Profile'))));
-    }, [dispatch]);
+    }, [dispatch,handleLogout,User?.token]);
 
 
   return (
